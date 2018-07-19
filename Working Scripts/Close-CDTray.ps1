567b291d25a3dd01e2d8d43c34f7bc9f6070a0b9
itsontheb <itsontheb@users.noreply.github.com>
2018-07-18 21:11:12 -0400

    Add Function: Close-CDTray
    Added function Close-CDTray.ps1 to faciliate the closing of CD Tray(s).

---
+++ Working Scripts/Close-CDTray.ps1
@@ -0,0 +1,56 @@
+﻿Function Close-CDTray
+{
+    # Figure out the type needed for $tray to work with $trays
+    # Go through comment to ensure instances for $tray type is added
+    <#.
+        .SYNOPSIS
+            Closes open CD Trays.
+        .DESCRIPTION
+            This function closes all CD Trays on the Local Machine. If the 
+            $Tray parameter is utilized it will only close that drive.
+        .EXAMPLE
+            PS> Close-CDTray -Tray ''
+        .Parameter Tray
+            Name of the tray to close. If not specified all trays are closed.
+            Required name of tray can be found from:
+
+        .NOTES
+            Minor Modifications from the below:
+            https://www.adamtheautomator.com/use-powershell-to-automate-burning-cds/
+    .#>
+    [cmdletbinding()]
+    param (
+		[Parameter(Mandatory = $True,
+					ValueFromPipeline = $True,
+					ValueFromPipelineByPropertyName = $True,
+					HelpMessage = 'What CD Tray would you like to eject')]
+		[string]$Tray          
+    )
+    
+    begin
+    {
+        $DiskMaster = New-Object -ComObject IMAPI2.MsftDiscMaster2
+        $DiscRecorder = New-Object -ComObject IMAPI2.MsftDiscRecorder2
+        
+        if ($Tray)
+        {
+            # Missing Action
+        }
+        if ( -not ( $id))
+        {
+            $id = $DiskMaster.Item(0)
+        }
+    }
+
+    process
+    {
+        $DiscRecorder.InitializeDiscRecorder($id)
+        $DiscRecorder.CloseTray()
+    }
+
+    end
+    {
+
+    }
+
+} # END Function Close CDTray
