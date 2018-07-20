0e5979b2307092449812910294e82c740ea2b789
itsontheb <itsontheb@users.noreply.github.com>
2018-07-19 21:52:57 -0400

    GUI modifcations

--- Working Scripts/Write-FinalizedCD_GUI.ps1
+++ Working Scripts/Write-FinalizedCD_GUI.ps1
@@ -1,23 +1,18 @@
-﻿<# This form was created using POSHGUI.com  a free online gui designer for PowerShell
-.NAME
-    Untitled
-#>
-
-Add-Type -AssemblyName System.Windows.Forms
+﻿Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
 #region begin GUI{ 
 
 $Form                            = New-Object system.Windows.Forms.Form
-$Form.ClientSize                 = '500,210'
-$Form.text                       = "Form"
+$Form.ClientSize                 = '500,250'
+$Form.text                       = "Powershell: Write-FinalizedCD"
 $Form.TopMost                    = $false
 
 $trayCBox                        = New-Object system.Windows.Forms.ComboBox
 $trayCBox.text                   = "$FILLER"
 $trayCBox.width                  = 250
 $trayCBox.height                 = 25
-$trayCBox.location               = New-Object System.Drawing.Point(15,40)
+$trayCBox.location               = New-Object System.Drawing.Point(15,85)
 $trayCBox.Font                   = 'Microsoft Sans Serif,10'
 
 $trayLbl                         = New-Object system.Windows.Forms.Label
@@ -25,7 +20,7 @@ $trayLbl.text                    = "Utilize which CD-Tray to burn media?"
 $trayLbl.AutoSize                = $true
 $trayLbl.width                   = 25
 $trayLbl.height                  = 10
-$trayLbl.location                = New-Object System.Drawing.Point(15,20)
+$trayLbl.location                = New-Object System.Drawing.Point(15,65)
 $trayLbl.Font                    = 'Microsoft Sans Serif,10,style=Bold'
 
 $folderLbl                       = New-Object system.Windows.Forms.Label
@@ -33,18 +28,18 @@ $folderLbl.text                  = "Select folder to burn to media"
 $folderLbl.AutoSize              = $true
 $folderLbl.width                 = 25
 $folderLbl.height                = 10
-$folderLbl.location              = New-Object System.Drawing.Point(15,80)
+$folderLbl.location              = New-Object System.Drawing.Point(14,130)
 $folderLbl.Font                  = 'Microsoft Sans Serif,10,style=Bold'
 
-$TextBox1                        = New-Object system.Windows.Forms.TextBox
-$TextBox1.multiline              = $false
-$TextBox1.width                  = 250
-$TextBox1.height                 = 20
-$TextBox1.location               = New-Object System.Drawing.Point(15,95)
-$TextBox1.Font                   = 'Microsoft Sans Serif,10'
+$folderTxtBox                    = New-Object system.Windows.Forms.TextBox
+$folderTxtBox.multiline          = $false
+$folderTxtBox.width              = 250
+$folderTxtBox.height             = 20
+$folderTxtBox.location           = New-Object System.Drawing.Point(14,150)
+$folderTxtBox.Font               = 'Microsoft Sans Serif,10'
 
 $optGroup                        = New-Object system.Windows.Forms.Groupbox
-$optGroup.height                 = 175
+$optGroup.height                 = 220
 $optGroup.width                  = 200
 $optGroup.text                   = "Options for burning media"
 $optGroup.location               = New-Object System.Drawing.Point(285,20)
@@ -53,17 +48,42 @@ $folderBtn                       = New-Object system.Windows.Forms.Button
 $folderBtn.text                  = "Browse"
 $folderBtn.width                 = 60
 $folderBtn.height                = 30
-$folderBtn.location              = New-Object System.Drawing.Point(30,145)
+$folderBtn.location              = New-Object System.Drawing.Point(31,195)
 $folderBtn.Font                  = 'Microsoft Sans Serif,10,style=Bold'
 
 $startBtn                        = New-Object system.Windows.Forms.Button
 $startBtn.text                   = "Burn Media"
 $startBtn.width                  = 125
 $startBtn.height                 = 50
-$startBtn.location               = New-Object System.Drawing.Point(125,135)
+$startBtn.location               = New-Object System.Drawing.Point(125,185)
 $startBtn.Font                   = 'Microsoft Sans Serif,10,style=Bold'
 
-$Form.controls.AddRange(@($trayCBox,$trayLbl,$folderLbl,$TextBox1,$optGroup,$folderBtn,$startBtn))
+$FinalizeChBox                   = New-Object system.Windows.Forms.CheckBox
+$FinalizeChBox.text              = "Finalize Media"
+$FinalizeChBox.AutoSize          = $false
+$FinalizeChBox.width             = 95
+$FinalizeChBox.height            = 20
+$FinalizeChBox.location          = New-Object System.Drawing.Point(30,20)
+$FinalizeChBox.Font              = 'Microsoft Sans Serif,10'
+
+$ejectChBox                      = New-Object system.Windows.Forms.CheckBox
+$ejectChBox.text                 = "Eject Tray"
+$ejectChBox.AutoSize             = $false
+$ejectChBox.width                = 95
+$ejectChBox.height               = 20
+$ejectChBox.location             = New-Object System.Drawing.Point(30,45)
+$ejectChBox.Font                 = 'Microsoft Sans Serif,10'
+
+$notifyChBox                     = New-Object system.Windows.Forms.CheckBox
+$notifyChBox.text                = "Completion Notification"
+$notifyChBox.AutoSize            = $false
+$notifyChBox.width               = 95
+$notifyChBox.height              = 20
+$notifyChBox.location            = New-Object System.Drawing.Point(30,70)
+$notifyChBox.Font                = 'Microsoft Sans Serif,10'
+
+$Form.controls.AddRange(@($trayCBox,$trayLbl,$folderLbl,$folderTxtBox,$optGroup,$folderBtn,$startBtn))
+$optGroup.controls.AddRange(@($FinalizeChBox,$ejectChBox,$notifyChBox))
 
 #region gui events {
 #endregion events }
